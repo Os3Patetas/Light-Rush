7a1946254f98b8f100534f21d2637a6314a6f2dd
xGuddha <gvrodrigues@minha.fag.edu.br>
2022-09-21 15:56:26 -0300

    Variacoes de obstaculo

--- Assets/_Project/Obstacle.cs
+++ Assets/_Project/Obstacle.cs
@@ -1,3 +1,7 @@
+using com.icypeak.spawner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace com.icypeak.obstacle
@@ -10,12 +14,32 @@ namespace com.icypeak.obstacle
 
         void Awake()
         {
-            _rb = GetComponent<Rigidbody2D>();    
+            _rb = GetComponent<Rigidbody2D>();
         }
 
         void Start()
         {
+            Spawner parentSpawner = this.GetComponentInParent<Spawner>();
+            int aux = 0;
+            foreach (var item in parentSpawner.CurrentSpawnOrder)
+            {
+                Instantiate(parentSpawner.SpawnableBlocks[item],
+                    this.transform.position + new Vector3(0, parentSpawner.PossibleBlockPositions[aux], 0),
+                    Quaternion.identity,
+                    this.transform);
+                aux++;
+            }
+            parentSpawner.UpdateCurrentSeed();
+
             _rb.velocity = Vector2.left * _baseSpeed;
+        }
+
+        void Update()
+        {
+            if(transform.position.x <= -12)
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 }
--- Assets/_Project/Spawner.cs
+++ Assets/_Project/Spawner.cs
@@ -1,17 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace com.icypeak.spawner
 {
     public class Spawner : MonoBehaviour
     {
-        [SerializeField] GameObject obstaclePrefab;
+        [SerializeField] GameObject spawnableObstacle;
         [SerializeField] float spawnCooldown;
         [SerializeField] bool isActive;
         float _timer;
 
+        public GameObject[] SpawnableBlocks;
+        public float[] PossibleBlockPositions;
+        List<int> _originalSpawnOrder;
+        public int[] CurrentSpawnOrder;
+
         void Start()
         {
             _timer = spawnCooldown;
+            _originalSpawnOrder = new List<int>();
+            for (int i = 0; i < SpawnableBlocks.Length; i++)
+            {
+                _originalSpawnOrder.Add(i);
+            }
+            UpdateCurrentSeed();
         }
 
         void Update()
@@ -22,8 +35,15 @@ namespace com.icypeak.spawner
             if (_timer <= 0f)
             {
                 _timer = spawnCooldown;
-                Instantiate(obstaclePrefab, this.transform.position, Quaternion.identity);
+                Instantiate(spawnableObstacle, this.transform.position, Quaternion.identity, this.transform);
             }
+        }
+
+        public void UpdateCurrentSeed()
+        {
+            var rnd = new System.Random();
+
+            CurrentSpawnOrder = _originalSpawnOrder.OrderBy(item => rnd.Next()).ToArray();
         }
     }
 }
