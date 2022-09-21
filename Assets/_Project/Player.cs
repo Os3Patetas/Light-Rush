24b4fde55b87f5f7dfc81ee879aeee87c8d4bf37
xGuddha <gvrodrigues@minha.fag.edu.br>
2022-09-21 00:26:47 -0300

    Namespace global e namespace para o script Player

--- Assets/_Project/Player.cs
+++ Assets/_Project/Player.cs
@@ -2,40 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace com.icypeak.player 
 {
-    Rigidbody2D _rb;
-
-    bool _isPressingScreen;
-    void Awake()
+    public class Player : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody2D>();
-    }
+        Rigidbody2D _rb;
 
-    void Update()
-    {
-        _isPressingScreen = Input.GetMouseButton(0);
-    }
-
-    void FixedUpdate()
-    {
-        _rb.velocity = new Vector2(_rb.velocity.x, Mathf.Clamp(_rb.velocity.y, -10, 10));
-
-        if (_isPressingScreen)
+        bool _isPressingScreen;
+        void Awake()
         {
-            _rb.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+            _rb = GetComponent<Rigidbody2D>();
         }
-    }
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag(this.tag))
+        void Update()
         {
-
+            _isPressingScreen = Input.GetMouseButton(0);
         }
-        else
+
+        void FixedUpdate()
         {
-            Destroy(this.gameObject);
+            _rb.velocity = new Vector2(_rb.velocity.x, Mathf.Clamp(_rb.velocity.y, -10, 10));
+
+            if (_isPressingScreen)
+            {
+                _rb.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag(this.tag))
+            {
+
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 }
+
