29ddbe5988f8a47f87bc9ff64ffcd071cf5e2fd9
gvrDev <guilhermevrodrigues123@gmail.com>
2022-10-19 13:44:27 -0300

    Leitura do Gold e Cash do Player no db

---
+++ Assets/Currency.cs
@@ -0,0 +1,13 @@
+
+
+public class Currency
+{
+    public int gold;
+    public int cash;
+
+    public Currency(int gold, int cash)
+    {
+        this.gold = gold;
+        this.cash = cash;
+    }
+}
